e0527780f4dea3237c559c6b0114b1212bb790bc
Alberto Guerra <aguerra@clarcat.com>
2023-10-03 12:34:51 +0200

    not used import fix

--- Videoclub.API/Controllers/UsersController.cs
+++ Videoclub.API/Controllers/UsersController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Videoclub.API.Context;
 using Videoclub.API.DTOs;
 using Videoclub.API.Model;
 using Videoclub.API.Services.Interfaces;
